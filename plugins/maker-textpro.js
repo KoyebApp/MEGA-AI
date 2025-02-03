2fae205d1894ba193a3084648f6cfeb51a3d3dda
Koyeb App <koyebapp@gmail.com>
2025-02-03 22:34:47 +0500

    Update maker-textpro.js

--- plugins/maker-textpro.js
+++ plugins/maker-textpro.js
@@ -75,24 +75,25 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
          throw 'Command not recognized.';
    }
 
-   // Fetch the image URL from the API
-   try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
+// Fetch the image URL from the API
+try {
+   const response = await fetch(apiUrl);
+   const data = await response.json();
 
-      if (data.success && data.result && data.result.imageUrl) {
-         // Send the image to the user
-         conn.sendFile(m.chat, data.result.imageUrl, 'logo.png', `*𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 © 𝙼𝙴𝙶𝙰-𝙰𝙸*`, m);
-         m.react(done);
-      } else {
-         throw 'Failed to generate the image. Please try again later.';
-      }
-   } catch (error) {
-      console.error('Error fetching image:', error);
-      m.reply('An error occurred while fetching the image. Please try again later.');
+   if (data.status && data.result && data.result.imageUrl) {
+      // Send the image to the user with the correct file extension
+      const imageUrl = data.result.imageUrl;
+      const fileExtension = imageUrl.split('.').pop(); // Extract the file extension
+      conn.sendFile(m.chat, imageUrl, `logo.${fileExtension}`, `*𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 © 𝙼𝙴𝙶𝙰-𝙰𝙸*`, m);
+      m.react(done);
+   } else {
+      throw 'Failed to generate the image. Please try again later.';
    }
+} catch (error) {
+   console.error('Error fetching image:', error);
+   m.reply('An error occurred while fetching the image. Please try again later.');
 }
-
+   
 handler.help = [
    'mascot', 'logomaker', 'bpstyle', 'writetext', 'glossy', 'cartoon', 'pixelglitch', 'advancedglow', 
    'lighteffect', 'texteffect', 'galaxy', 'beach', 'clouds'
